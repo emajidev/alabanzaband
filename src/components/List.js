bcc419f55d4e78976dd669ebd8144d5593b48449
emajidev <emajidev@gmail.com>
2019-10-20 20:22:08 -0400

    added push in db

--- src/components/List.js
+++ src/components/List.js
@@ -1,51 +1,90 @@
 import React, { Component } from 'react'
-import { Text, View, TouchableOpacity, StyleSheet,ScrollView,Alert} from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet,ScrollView,Alert,TextInput,TouchableHighlight} from 'react-native'
+import { db } from './db';
 
+let addItem = item => {
+   db.ref('/items').push({
+    name: item
+  })
+};
 class List extends Component {
-   state ={
-      names: [
-         {
-         id: 0,
-         name: 'Ben',
-         },
-         {
-         id: 1,
-         name: 'Susan',
-         },
-         {
-         id: 2,
-         name: 'Susan',
-         }
-      ]
-    }
+      state = {
+         name: ''
+      };
+   
+   handleChange = e => {
+         this.setState({
+         name: e.nativeEvent.text
+         });
+      };
+      handleSubmit = () => {
+         addItem(this.state.name);
+         alert('Item saved successfully');
+      };
     alertItemName = (item) => {
       alert(item.name)
-      }
+      };
     render() {
        return (
-          <View style = {styles.container}  >
-            <ScrollView >
-               {
-                  this.state.names.map((item, index) => (
-                  <TouchableOpacity
-                     key = {item.id}
-                     style = {styles.TouchableOpacity}
-                     onPress = {() => this.alertItemName(item)
-                  }>
-                  <Text style = {styles.text}>
-                     {item.name}
-                  </Text>
-                  </TouchableOpacity>
-                  ))
-               }
-            </ScrollView>
-          </View>
+    
+            <View style={styles.main}>
+            <Text style={styles.title}>Add Item</Text>
+            <TextInput style={styles.itemInput} onChange={this.handleChange} />
+            <TouchableHighlight
+               style={styles.button}
+               underlayColor="white"
+               onPress={this.handleSubmit}
+            >
+               <Text style={styles.buttonText}>Add</Text>
+            </TouchableHighlight>
+            </View>
+          
+       
        )
     }
  }
  export default List
 
  const styles = StyleSheet.create ({
+   main: {
+      flex: 1,
+      padding: 30,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      backgroundColor: '#6565fc'
+    },
+    title: {
+      marginBottom: 20,
+      fontSize: 25,
+      textAlign: 'center'
+    },
+    itemInput: {
+      height: 50,
+      padding: 4,
+      marginRight: 5,
+      fontSize: 23,
+      borderWidth: 1,
+      borderColor: 'white',
+      borderRadius: 8,
+      color: 'white'
+    },
+    buttonText: {
+      fontSize: 18,
+      color: '#111',
+      alignSelf: 'center'
+    },
+    button: {
+      height: 45,
+      flexDirection: 'row',
+      backgroundColor: 'white',
+      borderColor: 'white',
+      borderWidth: 1,
+      borderRadius: 8,
+      marginBottom: 10,
+      marginTop: 10,
+      alignSelf: 'stretch',
+      justifyContent: 'center'
+    },
      container:{
         flex:1,
         width: '100%',
---
+++ src/components/db.js
@@ -0,0 +1,13 @@
+import Firebase from 'firebase';
+let config = {
+    apiKey: "AIzaSyC9pPxtwaSkL8M-6T1tv6-ZYWtUmtoJlWs",
+    authDomain: "alabanzaband.firebaseapp.com",
+    databaseURL: "https://alabanzaband.firebaseio.com",
+    projectId: "alabanzaband",
+    storageBucket: "",
+    messagingSenderId: "1085755789248",
+    appId: "1:1085755789248:web:66c67449de5dc516cac225",
+    measurementId: "G-WWJHTYRF48"
+  };
+let app = Firebase.initializeApp(config);
+export const db = app.database();
